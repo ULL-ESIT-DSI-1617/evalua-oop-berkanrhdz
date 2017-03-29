c9f78585e36b44ba11b0fc6d0644733353a88b3d
berkanrhdz <alu0100815733@ull.edu.es>
2017-03-29 11:06:35 +0100

    Añadida herencia RTextCell

--- TextCell.js
+++ TextCell.js
@@ -51,3 +51,21 @@ class TextCell {
 
 // End TextCell
 }
+
+// RTextCell
+class RTextCell extends TextCell {
+  constructor(text) {
+    super(text);
+    this.text = text;
+  }
+
+  draw (width, height) {
+    var result = [];
+    for (var i = 0; i < height; i++) {
+      var line = this.text[i] || "";
+      result.push(" ".repeat(width - line.length) + line);
+    }
+    return result;
+  }
+// End RTextCell
+}
